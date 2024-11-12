14045546679483179fee3ef783cad864c620d89d
gangmaru <bigcocoa1@gachon.ac.kr>
2024-11-12 22:26:30 +0900

    week7 workbook

---
+++ cola/week7/src/main/java/umc/spring/domain/Alarm.java
@@ -0,0 +1,31 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Alarm extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "VARCHAR(30)")
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private Boolean optional;
+
+
+    //
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
